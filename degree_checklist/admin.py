85aea3e81529aada46bffb3dc11286cfa621b356
unflinching7 <daburkett1@buffs.wtamu.edu>
2023-11-29 21:48:14 -0800

    5

--- degree_checklist/admin.py
+++ degree_checklist/admin.py
@@ -1,77 +1,44 @@
 from django.contrib import admin
-from django import forms
 from .models import Student, DegreeProgram, Course, DegreeChecklistTemplate, StudentDegreeChecklist, CourseEnrollment
 
 # Register models
-admin.site.register(Student)
-admin.site.register(DegreeProgram)
 admin.site.register(Course)
 admin.site.register(DegreeChecklistTemplate)
 admin.site.register(StudentDegreeChecklist)
 admin.site.register(CourseEnrollment)
 
+# Add import_data function outside the admin classes
+def import_data(modeladmin, request, queryset):
+    # data import logic for the model here
+    # This function will be called when the "Import Data" action is selected in the admin interface
+    messages.success(request, "Data imported successfully.")
+    return redirect('admin:index')
+
+# Short description for the import_data action
+import_data.short_description = "Import Data"
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    actions = ['import_data']
-
-    def import_data(self, request, queryset):
-        # data import logic for the Student model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
-
-    import_data.short_description = "Import Data"
-
+    actions = [import_data]
 
 @admin.register(DegreeProgram)
 class DegreeProgramAdmin(admin.ModelAdmin):
-    actions = ['import_data']
-
-    def import_data(self, request, queryset):
-        # data import logic for the DegreeProgram model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
-
-    import_data.short_description = "Import Data"
-
+    actions = [import_data]
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    actions = ['import_data']
-
-    def import_data(self, request, queryset):
-        # data import logic for the Course model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
-
-    import_data.short_description = "Import Data"
-
+    actions = [import_data]
 
 @admin.register(DegreeChecklistTemplate)
 class DegreeChecklistTemplateAdmin(admin.ModelAdmin):
-    actions = ['import_data']
-
-    def import_data(self, request, queryset):
-        # data import logic for the DegreeChecklistTemplate model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
-
-    import_data.short_description = "Import Data"
-
+    actions = [import_data]
 
 @admin.register(StudentDegreeChecklist)
 class StudentDegreeChecklistAdmin(admin.ModelAdmin):
-    actions = ['import_data']
-
-    def import_data(self, request, queryset):
-        # data import logic for the StudentDegreeChecklist model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
-
-    import_data.short_description = "Import Data"
-
+    actions = [import_data]
 
 @admin.register(CourseEnrollment)
 class CourseEnrollmentAdmin(admin.ModelAdmin):
-    actions = ['import_data']
+    actions = [import_data]
 
-    def import_data(self, request, queryset):
-        # data import logic for the CourseEnrollment model here
-        # This function will be called when the "Import Data" action is selected in the admin interface
 
-    import_data.short_description = "Import Data"
